4da4d30490b2ed8ae2bd029f1bfce183b134ea1a
TalKashi <shakikashi@gmail.com>
2015-05-14 01:25:10 +0300

    Player

---
+++ B15 Ex02 Serge 310881082 Tal 200348316/B15 Ex02 Serge 310881082 Tal 200348316/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B15_Ex02_Serge_310881082_Tal_200348316
+{
+    class Player
+    {
+        private readonly string r_Name;
+        private readonly Piece r_Color;
+
+        public Player(string i_Name, Piece i_Color)
+        {
+            r_Name = i_Name;
+            r_Color = i_Color;
+        }
+    }
+}
