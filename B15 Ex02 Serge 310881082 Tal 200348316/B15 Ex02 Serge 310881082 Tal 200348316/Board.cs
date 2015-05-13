012e93f4eeca19ce2c481cb378764e74c0d3dbc1
SergeOxe <sergeoxe@gmail.com>
2015-05-13 18:20:33 +0300

    Sync

---
+++ B15 Ex02 Serge 310881082 Tal 200348316/B15 Ex02 Serge 310881082 Tal 200348316/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B15_Ex02_Serge_310881082_Tal_200348316
+{
+    class Board
+    {
+        private int m_boardSize;
+
+        Board(int i_boardSize)
+        {
+            m_boardSize = i_boardSize;
+
+        }
+
+
+        public void DrawBoard()
+        {
+            
+        }
+
+
+
+    }
+}
